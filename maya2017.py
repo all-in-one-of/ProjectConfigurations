ca89bac80b27341d350b61e92eb63f7ec9402004
MCL <1204172445@qq.com>
2019-04-30 17:30:42 +0800

    --start new project configuration

---
+++ maya2017.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Time      :  12:08
+# Email     : spirit_az@foxmail.com
+# File      : maya2017.py
+__author__ = 'ChenLiang.Miao'
+
+# --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+--#
+import scriptTool
+
+FILENAME = scriptTool.getFile(__file__)
+basePath = scriptTool.getScriptPath()
+# --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+--#
+ProjectName = ""
+PipelineSoftware = ''
+Softwares = ['MAYA', 'HOUDINI', 'SUBSTANCEPANTER', 'PHOTOSHOP']
+
+# --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+--#
+FPS = 25
+UNIT = 'cm'
+Renderer = 'arnold5.3'
+
+# --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+--#
+MAYA_VERSION = 2017
+MAYAMenu = [{'General': {}},
+            {'Model': [['scene optimize', 'from GNL.Check import openUI;reload(openUI);openUI.show(1, "Model")'],
+
+                       ]},
+            {'LookDiv': {}},
+            {'Rigging': {}},
+            {'CFX': {}},
+            {'EFX': {}},
+            {'Track': {}},
+            {'Layout': {}},
+            {'Animate': {}},
+            {'Environment': {}},
+            {'Lighting': {}},
+            {'Help': {}}]
+
+MAYAPluginList = ['AbcExport.mll', 'AbcImport.mll', 'matrixNodes.mll', 'gpuCache.mll',
+                  'fbxmaya.mll', 'objExport.mll', 'cgfxShader.mll',
+                  'invertShape.mll', 'poseInterpolator.mll', 'projectConfiguration.py']
+
+MAYAToolVariables = {
+    'MAYA_SCRIPT_PATH': ['{}/Maya/Tools'.format(basePath),
+                         '{}/Maya/Tools/ANI'.format(basePath),
+                         '{}/Maya/Tools/CFX'.format(basePath),
+                         '{}/Maya/Tools/EFX'.format(basePath),
+                         '{}/Maya/Tools/ENV'.format(basePath),
+                         '{}/Maya/Tools/GNL'.format(basePath),
+                         '{}/Maya/Tools/LAY'.format(basePath),
+                         '{}/Maya/Tools/LDV'.format(basePath),
+                         '{}/Maya/Tools/LGT'.format(basePath),
+                         '{}/Maya/Tools/MOD'.format(basePath),
+                         '{}/Maya/Tools/RIG'.format(basePath),
+                         '{}/Maya/Tools/TRK'.format(basePath),
+
+                         ],
+    'MAYA_PLUG_IN_PATH': []
+}
+
+MAYA_BAT_PATH = {"MAYA_MODULE_PATH": ["{}/Maya/modules/MCLShelf/MCLShelf1.0".format(basePath),
+                                      "{}/Maya/modules/SHAPESBrush/2017/SHAPESBrush1.0".format(basePath),
+                                      "{}/Maya/modules/TheSetupMachine/TSM3".format(basePath),
+                                      "{}/Maya/modules/MCLRig/MCLRig1.0".format(basePath),
+                                      ],
+                 'PYTHONPATH': ['{}/Maya/Python/2.7/Lib/site-packages'.format(basePath),
+                                '{}/Maya/Tools'.format(basePath),
+                                '{}/Maya/Scritps'.format(basePath)],
+
+                 }
+MAYA_LOCATION = """
+"D:/Program Files/Autodesk/Maya{0}/bin/maya.exe" -noAutoloadPlugins -command "loadPlugin //"projectConfiguration.py//";projectConf -ls all -lp //"{1}//";"
+""".format(MAYA_VERSION, FILENAME)
+
+# --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+--#
+HOUDINI_VERSION = "17.0.416"
+HOUDINIMenu = [{'General': {}},
+               {'Help': {}}]
+
+HOUDINIToolVariables = {
+}
+
+HOUDINI_BAT_PATH = {}
+HOUDINI_LOCATION = """
+"D:/Program Files/Side Effects Software/Houdini {0}/bin/houdinifx.exe"
+""".format(HOUDINI_VERSION)
+
+# --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+--#
+SUBSTANCE_VERSION = ''
+
+SUBSTANCE_BAT_PATH = {}
+
+SUBSTANCE_LOCATION = """
+"D:/Program Files/Allegorithmic/Substance Painter/Substance Painter.exe"
+"""
---
+++ scriptTool.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Time      :  17:14
+# Email     : spirit_az@foxmail.com
+# File      : scriptTool.py
+__author__ = 'ChenLiang.Miao'
+
+# import --+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+ #
+
+import os, inspect
+
+
+# function +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+ #
+
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+ #
+def getParentClasses(obj):
+    """
+    Get object's all of parent class...
+    """
+    if type(obj) == type:
+        return inspect.getmro(obj)
+    else:
+        return inspect.getmro(obj.__class__)
+
+
+def getFile(module, basename=True):
+    if not basename:
+        return module
+    return os.path.splitext(os.path.basename(module))[0]
+
+
+def getModulesPath(module):
+    """
+    return dir for imported module..
+    """
+    moduleFile = inspect.getfile(module)
+    modulePath = os.path.dirname(moduleFile)
+    return modulePath
+
+
+def getScriptPath():
+    """
+    return dir path for used script..
+    """
+    scriptPath = getModulesPath(inspect.currentframe().f_back)
+    return scriptPath
